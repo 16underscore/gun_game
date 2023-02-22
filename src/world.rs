c87e908478df2a88018c7c39b7a5e2a4328eb3cb
16_ <steffen.gminsky@online.de>
2023-02-22 16:01:32 +0100

    improve code

--- src/world.rs
+++ src/world.rs
@@ -9,19 +9,19 @@ pub fn setup(world: &mut World) {
 	let mut anvil = AnvilWorld::new("world");
 	for x in -2..2 {
 		for z in -2..2 {
-			if let Ok(Some(anvil_chunk)) = anvil.read_chunk(x, z) {
-				let mut chunk = Chunk::new(24);
-				if let Err(_) =
-					valence_anvil::to_valence(&anvil_chunk.data, &mut chunk, 4, |_| BiomeId::default())
-				{
-					instance.insert_chunk([x, z], Chunk::default());
-				} else {
-					instance.insert_chunk([x, z], chunk);
-				}
+			if let Some(chunk) = get_chunk(x, z, &mut anvil) {
+				instance.insert_chunk([x, z], chunk);
 			} else {
 				instance.insert_chunk([x, z], Chunk::default());
 			}
 		}
 	}
 	world.spawn(instance);
+}
+
+fn get_chunk(x: i32, z: i32, anvil: &mut AnvilWorld) -> Option<Chunk> {
+	let anvil_chunk = anvil.read_chunk(x, z).ok()??;
+	let mut chunk = Chunk::new(24);
+	valence_anvil::to_valence(&anvil_chunk.data, &mut chunk, 4, |_| BiomeId::default()).ok()?;
+	Some(chunk)
 }
